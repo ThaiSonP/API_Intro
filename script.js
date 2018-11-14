f64eb7b447ea052d418fea5ff2b402ae987c2135
ThaiSonP <tysonpan@pursuit.org>
2018-11-14 14:56:20 -0500

    i did not finish

---
+++ script.js
@@ -0,0 +1,195 @@
+const APIResponse = {
+    "status": "success",
+    "message": {
+        "affenpinscher": [],
+        "african": [],
+        "airedale": [],
+        "akita": [],
+        "appenzeller": [],
+        "basenji": [],
+        "beagle": [],
+        "bluetick": [],
+        "borzoi": [],
+        "bouvier": [],
+        "boxer": [],
+        "brabancon": [],
+        "briard": [],
+        "bulldog": [
+            "boston",
+            "french"
+        ],
+        "bullterrier": [
+            "staffordshire"
+        ],
+        "cairn": [],
+        "cattledog": [
+            "australian"
+        ],
+        "chihuahua": [],
+        "chow": [],
+        "clumber": [],
+        "cockapoo": [],
+        "collie": [
+            "border"
+        ],
+        "coonhound": [],
+        "corgi": [
+            "cardigan"
+        ],
+        "cotondetulear": [],
+        "dachshund": [],
+        "dalmatian": [],
+        "dane": [
+            "great"
+        ],
+        "deerhound": [
+            "scottish"
+        ],
+        "dhole": [],
+        "dingo": [],
+        "doberman": [],
+        "elkhound": [
+            "norwegian"
+        ],
+        "entlebucher": [],
+        "eskimo": [],
+        "frise": [
+            "bichon"
+        ],
+        "germanshepherd": [],
+        "greyhound": [
+            "italian"
+        ],
+        "groenendael": [],
+        "hound": [
+            "afghan",
+            "basset",
+            "blood",
+            "english",
+            "ibizan",
+            "walker"
+        ],
+        "husky": [],
+        "keeshond": [],
+        "kelpie": [],
+        "komondor": [],
+        "kuvasz": [],
+        "labrador": [],
+        "leonberg": [],
+        "lhasa": [],
+        "malamute": [],
+        "malinois": [],
+        "maltese": [],
+        "mastiff": [
+            "bull",
+            "tibetan"
+        ],
+        "mexicanhairless": [],
+        "mix": [],
+        "mountain": [
+            "bernese",
+            "swiss"
+        ],
+        "newfoundland": [],
+        "otterhound": [],
+        "papillon": [],
+        "pekinese": [],
+        "pembroke": [],
+        "pinscher": [
+            "miniature"
+        ],
+        "pointer": [
+            "german",
+            "germanlonghair"
+        ],
+        "pomeranian": [],
+        "poodle": [
+            "miniature",
+            "standard",
+            "toy"
+        ],
+        "pug": [],
+        "puggle": [],
+        "pyrenees": [],
+        "redbone": [],
+        "retriever": [
+            "chesapeake",
+            "curly",
+            "flatcoated",
+            "golden"
+        ],
+        "ridgeback": [
+            "rhodesian"
+        ],
+        "rottweiler": [],
+        "saluki": [],
+        "samoyed": [],
+        "schipperke": [],
+        "schnauzer": [
+            "giant",
+            "miniature"
+        ],
+        "setter": [
+            "english",
+            "gordon",
+            "irish"
+        ],
+        "sheepdog": [
+            "english",
+            "shetland"
+        ],
+        "shiba": [],
+        "shihtzu": [],
+        "spaniel": [
+            "blenheim",
+            "brittany",
+            "cocker",
+            "irish",
+            "japanese",
+            "sussex",
+            "welsh"
+        ],
+        "springer": [
+            "english"
+        ],
+        "stbernard": [],
+        "terrier": [
+            "american",
+            "australian",
+            "bedlington",
+            "border",
+            "dandie",
+            "fox",
+            "irish",
+            "kerryblue",
+            "lakeland",
+            "norfolk",
+            "norwich",
+            "patterdale",
+            "russell",
+            "scottish",
+            "sealyham",
+            "silky",
+            "tibetan",
+            "toy",
+            "westhighland",
+            "wheaten",
+            "yorkshire"
+        ],
+        "vizsla": [],
+        "weimaraner": [],
+        "whippet": [],
+        "wolfhound": [
+            "irish"
+        ]
+    }
+}
+
+let breedList = [];
+breedList.push(APIResponse['message'])
+
+let words = "hello world"
+console.log(words.toUpperCase())
+
+// console.log(APIResponse['message'])
+// console.log(breedList.length)
